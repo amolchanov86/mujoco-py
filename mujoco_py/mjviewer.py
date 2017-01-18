679a8a00899b4ab78ff62d1b0a3193080141305c
Joe Mathai <joemathai16@gmail.com>
2017-01-17 16:41:29 -0800

    bug fix

--- mujoco_py/mjviewer.py
+++ mujoco_py/mjviewer.py
@@ -96,7 +96,15 @@ class MjViewer(object):
         rect = self.get_rect()
         arr = (ctypes.c_double*3)(0, 0, 0)
 
+
         mjlib.mjv_makeGeoms(self.model.ptr, self.data.ptr, byref(self.objects), byref(self.vopt), mjCAT_ALL, 0, None, None, ctypes.cast(arr, ctypes.POINTER(ctypes.c_double)))
+
+        g = self.objects.geoms[self.objects.ngeom]
+        print g.type, g.objtype, g.pos[0],g.pos[1], g.pos[2]
+        self.v_defaultGeom(g)
+        self.objects.ngeom += 1
+
+
         mjlib.mjv_makeLights(self.model.ptr, self.data.ptr, byref(self.objects))
 
         mjlib.mjv_setCamera(self.model.ptr, self.data.ptr, byref(self.cam))
@@ -333,13 +341,13 @@ class MjViewer(object):
         mjlib.mjv_freeObjects(byref(self.objects))
         self.running = False
 
-    def v_defaultGeom():
-        g = mjcore.MJVGEOM()
-        g.type = mjGEOM_ARROW
+    def v_defaultGeom(self, g):
+        #pass in a mjvGeom object
+        g.type = mjconstants.mjGEOM_ARROW
         g.dataid = -1
-        g.objtype = mjOBJ_UNKNOWN
+        g.objtype = mjconstants.mjOBJ_UNKNOWN
         g.objid = -1
-        g.category = mjCAT_DECOR
+        g.category = mjconstants.mjCAT_DECOR
         g.texid = -1
         g.texuniform = 0
         g.texrepeat[0] = 1
@@ -348,10 +356,12 @@ class MjViewer(object):
         g.specular = 0.5
         g.shininess = 0.5
         g.reflectance = 0
-        g.label[0] = 0
-        g.size[0] = g.size[1] = 0.01
-        g.size[2] = 0.05
-        print 'test v_defaultGeom'
-        g.pos[0]=g.pos[1]=g.pos[2]=0.0
+        g.size[0] = g.size[1] = 0.09
+        g.size[2] = 0.09
+        g.rgba[0] = 1
+        g.rgba[1] = 0
+        g.rgba[2] = 0
+        g.rgba[3] = 1
+        g.pos[0]=g.pos[1]=g.pos[2]=0.05
         
         
